2c90ea2a4db9b69c656f67731bbe58b951e0c79f
G3nKai <kulikovila2005@gmail.com>
2025-04-03 04:06:21 +0700

    создана первая модель

---
+++ src/main/java/com/java/todolist/core/domain/task.java
@@ -0,0 +1,45 @@
+package com.java.todolist.core.domain;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @NotNull
+    private String name;
+    @NotNull
+    private String description;
+
+    public task(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
