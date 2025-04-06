a0920ab4ab6aaa4f19cba19db1f52072ca348355
G3nKai <kulikovila2005@gmail.com>
2025-04-06 17:19:34 +0700

    создаются задачи. Только имя. Базовый стиль

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,18 +1,63 @@
-function getAllTasks() {
-    fetch("http://localhost:8080/tasks", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
-    .then(response => response.json())
-    .then(data => 
-        data.forEach(element => {
-            console.log(element);
-        })
+async function getAllTasks(parent) {
+    try {
+        const response = await fetch("http://localhost:8080/tasks", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const data = await response.json();
 
-    )
-    .catch(error => console.error("Ошибка getAllTasks()"));
+        const ul = document.createElement('ul');
+        ul.className = "list-group shadow-sm";
+
+        data.forEach(element => {
+
+            const task = {
+                "id": element.id,
+                "name": element.name,
+                "description": element.description,
+                "deadline": element.deadline,
+                "status": element.status,
+                "priority": element.priority,
+                "created": element.created,
+                "edited": element.edited
+            }
+
+            const li = document.createElement('li');
+            li.className = "list-group-item";
+            li.textContent = task.name;
+            ul.appendChild(li);
+        });
+
+        parent.appendChild(ul);
+        
+    } catch (error) {
+        console.error("Ошибка getAllTasks()");
+    }
 }
 
-getAllTasks();
+async function firstLoad() {
+    const body = document.querySelector('body');
+    body.className = "bg-light";
+
+    const div1 = document.createElement('div');
+    div1.className = "container py-5";
+
+    body.appendChild(div1);
+
+    const mainH1 = document.createElement('h1');
+    mainH1.className = "text-center mb-4";
+    mainH1.textContent = "Мои задачи";
+    div1.appendChild(mainH1);
+
+    const div2 = document.createElement('div');
+    div2.className = "task-wrapper";
+
+    div1.appendChild(div2);
+
+    await getAllTasks(div2);
+    
+}
+
+firstLoad();
